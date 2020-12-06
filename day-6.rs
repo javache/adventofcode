e5cf4ef46c270760bc952e8143737b5f5ea8ff24
Pieter De Baets <pieter.debaets@gmail.com>
2020-12-06 14:58:04 +0000

    day-6

---
+++ day-6.rs
@@ -0,0 +1,32 @@
+use std::collections::HashSet;
+use std::io::{self, Read};
+
+fn main() -> io::Result<()> {
+    let mut any_sum = 0;
+    let mut all_sum = 0;
+
+    let mut input = String::new();
+    io::stdin().lock().read_to_string(&mut input).unwrap();
+    for group in input.split("\n\n") {
+        let mut any_answers: HashSet<char> = HashSet::new();
+        let mut all_answers: HashSet<char> = HashSet::new();
+
+        for (i, member) in group.split_whitespace().enumerate() {
+            let answers: HashSet<char> = member.chars().collect();
+            any_answers.extend(&answers);
+            if i == 0 {
+                all_answers = answers;
+            } else {
+                all_answers = all_answers.intersection(&answers).cloned().collect();
+            }
+        }
+
+        any_sum += any_answers.len();
+        all_sum += all_answers.len();
+    }
+
+    println!("(1) Sum is {}", any_sum);
+    println!("(2) Sum is {}", all_sum);
+
+    Ok(())
+}
