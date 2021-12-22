bfece0a27febe3426cc0269170f08dae1b4d1536
Pieter De Baets <pieter.debaets@gmail.com>
2021-12-22 23:20:44 +0100

    Day 22 (simpler solution)

--- 2021/src/day-22.rs
+++ 2021/src/day-22.rs
@@ -1,162 +1,49 @@
-use itertools::Itertools;
 use regex::Regex;
+use std::collections::HashMap;
 use std::io::{self, BufRead};
 
 type Point = (i32, i32, i32);
 type Cuboid = (Point, Point);
 type Instruction = (Cuboid, bool);
-type State = Vec<(i32, Vec<(i32, Vec<(i32, bool)>)>)>;
 
-fn split_range(splits: &mut Vec<i32>, point_1: i32, point_2: i32) {
-    match splits.binary_search(&point_1) {
-        Ok(_) => (),
-        Err(pos) => splits.insert(pos, point_1),
-    }
-    match splits.binary_search(&point_2) {
-        Ok(_) => (),
-        Err(pos) => splits.insert(pos, point_2),
-    }
+fn cuboid_intersection(a: &Cuboid, b: &Cuboid) -> Option<Cuboid> {
+    let x0 = a.0 .0.max(b.0 .0);
+    let x1 = a.1 .0.min(b.1 .0);
+    let y0 = a.0 .1.max(b.0 .1);
+    let y1 = a.1 .1.min(b.1 .1);
+    let z0 = a.0 .2.max(b.0 .2);
+    let z1 = a.1 .2.min(b.1 .2);
+
+    (x0 <= x1 && y0 <= y1 && z0 <= z1).then(|| ((x0, y0, z0), (x1, y1, z1)))
 }
 
-fn get_point_state(state: &State, point: &Point) -> bool {
-    let x_idx = match state.binary_search_by_key(&point.0, |split| split.0) {
-        Ok(idx) => idx,
-        Err(idx) => idx,
-    };
-    if x_idx == state.len() {
-        return false;
-    }
+fn solve(instructions: &Vec<Instruction>) -> usize {
+    let mut state: HashMap<Cuboid, i32> = HashMap::new();
 
-    let y_splits = &state[x_idx].1;
-    let y_idx = match y_splits.binary_search_by_key(&point.1, |split| split.0) {
-        Ok(idx) => idx,
-        Err(idx) => idx,
-    };
-    if y_idx == y_splits.len() {
-        return false;
-    }
-
-    let z_splits = &y_splits[y_idx].1;
-    let z_idx = match z_splits.binary_search_by_key(&point.2, |split| split.0) {
-        Ok(idx) => idx,
-        Err(idx) => idx,
-    };
-    if z_idx == z_splits.len() {
-        false
-    } else {
-        z_splits[z_idx].1
-    }
-}
-
-fn evaluate_instructions(cuboid: &Cuboid, instructions: &Vec<Instruction>) -> usize {
-    let mut state: State = vec![];
-    for (i, instr) in instructions.iter().enumerate() {
-        println!("Processing instruction {}", i + 1);
-        let (range_min, range_max) = (instr.0 .0, instr.0 .1);
-
-        let mut x_splits: Vec<i32> = state.iter().map(|split| split.0).collect();
-        split_range(&mut x_splits, range_min.0 - 1, range_max.0);
-
-        // let mut y_splits: Vec<i32> = state
-        //     .iter()
-        //     .map(|split| split.1.iter().map(|split| split.0))
-        //     .flatten()
-        //     .unique()
-        //     .collect();
-        let mut y_splits: Vec<i32> = if state.len() > 0 {
-            state[0].1.iter().map(|split| split.0).collect()
-        } else {
-            vec![]
-        };
-        split_range(&mut y_splits, range_min.1 - 1, range_max.1);
-
-        let mut z_splits: Vec<i32> = state
-            .iter()
-            .map(|split| {
-                split
-                    .1
-                    .iter()
-                    .map(|split| split.1.iter().map(|split| split.0))
-                    .flatten()
-            })
-            .flatten()
-            .unique()
-            .sorted()
-            .collect();
-        // let mut z_splits: Vec<i32> = if state.len() > 0 {
-        //     state[0].1[0].1.iter().map(|split| split.0).collect()
-        // } else { vec![] };
-        split_range(&mut z_splits, range_min.2 - 1, range_max.2);
-
-        // TODO: consider using BTreeMap
-        // type State = HashMap<i32, HashMap<i32, HashMap<i32, bool>>>
-        // TODO: could merge contiguous splits
-        // TODO: we only need to re-evaluate splits within the instruction window
-        // TODO: use chain+windows to iterate
-        let mut prev_x_split = i32::MIN;
-        let mut x_states = vec![];
-        for x_split in &x_splits {
-            // Avoid re-evaluating splits outside the instruction window
-            // FIXME: we need to check all axis are outside the window
-            // if *x_split - 1 < range_min.0 && *x_split > range_max.0 {
-            //     let old_split = state.iter().find(|split| split.0 == *x_split).unwrap();
-            //     x_states.push(old_split.clone());
-            //     continue;
-            // }
-
-            let mut prev_y_split = i32::MIN;
-            let mut y_states = vec![];
-            for y_split in &y_splits {
-                let mut prev_z_split = i32::MIN;
-                let mut z_states: Vec<(i32, bool)> = vec![];
-                for z_split in &z_splits {
-                    let value = if prev_x_split >= range_min.0
-                        && *x_split <= range_max.0
-                        && prev_y_split >= range_min.1
-                        && *y_split <= range_max.1
-                        && prev_z_split >= range_min.2
-                        && *z_split <= range_max.2
-                    {
-                        instr.1
-                    } else {
-                        get_point_state(&state, &(*x_split, *y_split, *z_split))
-                    };
-
-                    // Merge continuous splits
-                    // if z_states[..].last().map_or(true, |last| last.1 != value) {
-                    z_states.push((*z_split, value));
-                    // } else {
-                    //     let last_idx = z_states.len() - 1;
-                    //     z_states[last_idx].0 = *z_split;
-                    // }
-                    prev_z_split = *z_split + 1;
-                }
-                y_states.push((*y_split, z_states));
-                prev_y_split = *y_split + 1;
-            }
-            x_states.push((*x_split, y_states));
-            prev_x_split = *x_split + 1;
-        }
-        state = x_states;
-    }
-
-    let mut count = 0;
-    // TODO: use windows to iterate
-    for (x_idx, x_split) in state.iter().enumerate().skip(1) {
-        for (y_idx, y_split) in x_split.1.iter().enumerate().skip(1) {
-            for (z_idx, z_split) in y_split.1.iter().enumerate().skip(1) {
-                if z_split.1 {
-                    let prev_x_split = state[x_idx - 1].0;
-                    let prev_y_split = x_split.1[y_idx - 1].0;
-                    let prev_z_split = y_split.1[z_idx - 1].0;
-                    count += (x_split.0 - prev_x_split) as usize
-                        * (y_split.0 - prev_y_split) as usize
-                        * (z_split.0 - prev_z_split) as usize
-                }
+    for instr in instructions.iter() {
+        let mut updates: HashMap<Cuboid, i32> = HashMap::new();
+        for (cuboid, value) in state.iter() {
+            if let Some(intersection) = cuboid_intersection(&cuboid, &instr.0) {
+                *updates.entry(intersection).or_default() -= value;
             }
         }
+        if instr.1 {
+            *updates.entry(instr.0).or_default() += 1;
+        }
+        for (cuboid, value) in updates.iter() {
+            *state.entry(*cuboid).or_default() += value;
+        }
     }
-    count
+
+    state
+        .iter()
+        .map(|(cuboid, value)| {
+            *value as usize
+                * (cuboid.1 .0 - cuboid.0 .0 + 1) as usize
+                * (cuboid.1 .1 - cuboid.0 .1 + 1) as usize
+                * (cuboid.1 .2 - cuboid.0 .2 + 1) as usize
+        })
+        .sum()
 }
 
 fn main() -> io::Result<()> {
@@ -177,8 +64,18 @@ fn main() -> io::Result<()> {
         })
         .collect();
 
-    let count = evaluate_instructions(&((-50, -50, -50), (50, 50, 50)), &instructions);
-    println!("(1) There are {} cubes lit up", count);
+    let window = ((-50, -50, -50), (50, 50, 50));
+    let limited_instructions: Vec<Instruction> = instructions
+        .iter()
+        .filter(|(cuboid, _)| cuboid_intersection(cuboid, &window).is_some())
+        .cloned()
+        .collect();
+    println!(
+        "(1) There are {} cubes lit up in x=-50..50,y=-50..50,z=-50..50",
+        solve(&limited_instructions)
+    );
+
+    println!("(2) There are {} cubes lit up", solve(&instructions));
 
     Ok(())
 }
